22d724678662465688197d007042fb336d06971e
s33588 <s33588@pjwstk.edu.pl>
2026-03-25 19:31:25 +0100

    Add a rental class

---
+++ APBD_proj_1/UniversityEquipmentRental/Models/Rental.cs
@@ -0,0 +1,33 @@
+namespace UniversityEquipmentRental.Models;
+
+public class Rental(int id, User user, Equipment equipment, DateTime rentDate, int days)
+{
+    public int Id { get; } = id;
+    public User User { get; } = user;
+    public Equipment Equipment { get; } = equipment;
+    public DateTime RentDate { get; } = rentDate;
+    private DateTime DueDate { get; } = rentDate.AddDays(days);
+    private DateTime? ReturnDate { get; set; }
+    public decimal Penalty { get; private set; }
+
+    public bool IsReturned => ReturnDate.HasValue;
+
+    public bool IsOverdue
+    {
+        get
+        {
+            if (IsReturned)
+            {
+                return ReturnDate!.Value.Date > DueDate.Date;
+            }
+
+            return DateTime.Now.Date > DueDate.Date;
+        }
+    }
+
+    public void Return(DateTime returnDate, decimal penalty)
+    {
+        ReturnDate = returnDate;
+        Penalty = penalty;
+    }
+}
